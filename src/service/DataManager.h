8b448353a7e57703e6145ecf24658f152475fd53
Wo-m <dom.thompson458@gmail.com>
2024-05-26 10:05:43 +1000

    install sqlite-orm

--- src/service/DataManager.h
+++ src/service/DataManager.h
@@ -8,12 +8,14 @@
 class DataManager {
 public:
     DataManager();
-    static void write(std::vector<Transaction> transactions);
     static Stats calculate_stats(std::vector<Transaction> transactions);
-    static void add_new_transaction(Transaction&);
     static void snapshot(int choice, bool);
-    static std::vector<Transaction> find_transactions(const date::year_month_day&, const date::year_month_day&, bool);
     static void calculate_saved(std::vector<Account>);
+
+    // everthing here can be moved to sql
+    static void write(std::vector<Transaction> transactions);
+    static void add_new_transaction(Transaction&);
+    static std::vector<Transaction> find_transactions(const date::year_month_day&, const date::year_month_day&, bool);
 
     static void AdHoc();
 };
