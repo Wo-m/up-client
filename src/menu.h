d361ccd9293cca374ed2583fa8b7d9abc1713cc7
Wo-m <dom.thompson458@gmail.com>
2024-09-27 14:07:54 +1000

    rename menu methods

--- src/menu.h
+++ src/menu.h
@@ -1,12 +1,9 @@
 
 #pragma once
-#include "config/Config.h"
 #include "model/Tags.h"
 #include "model/Transaction.h"
 #include "service/DataManager.h"
 #include "service/DateHelper.h"
-#include "service/Repository.h"
-#include "service/UpService.h"
 #include <date/date.h>
 #include <fmt/core.h>
 #include <iostream>
@@ -15,16 +12,16 @@
 
 using namespace std;
 
-class Menu
+vclass Menu
 {
 public:
     void main()
     {
-        start_up();
+        StartUp();
         string input;
         while (1)
         {
-            input = get_input(fmt::format("{}\n{}\n{}\n{}\n{}\n",
+            input = GetInput(fmt::format("{}\n{}\n{}\n{}\n{}\n",
                        "1: add new transactions",
                        "2: snapshots",
                        "-1: AdHoc",
@@ -38,10 +35,10 @@ public:
                     DataManager::AdHoc();
                     break;
                 case 1:
-                    add_new_transaction();
+                    AddTransaction();
                     break;
                 case 2:
-                    snapshot_menu();
+                    Snapshots();
                     break;
                 default:
                     fmt::print("not an option, try again\n");
@@ -53,7 +50,7 @@ public:
 private:
     DataManager data_manager_;
 
-    string get_input(std::string question, std::string default_choice = "")
+    string GetInput(std::string question, std::string default_choice = "")
     {
         fmt::print("{}\n", question);
         string input;
@@ -65,33 +62,28 @@ private:
         return input;
     }
 
-    void start_up()
-    {
-        find_new_transactions();
-    }
-
-    void find_new_transactions()
+    void StartUp()
     {
         data_manager_.UpdateTransactions(false);
     }
 
-    void add_new_transaction()
+    void AddTransaction()
     {
-        auto date = DateHelper::ToYearMonthDay(get_input("date (dd/mm/yy) (empty for today)", DateHelper::ToStringDDMMYY(DateHelper::GetToday())));
-        auto amount = get_input("amount (in cents: 10000 -> $100.00)");
-        auto description = get_input("description");
-        auto tag = tag_from_string(get_input("tag (empty for none)", "NONE"));
+        auto date = DateHelper::ToYearMonthDay(GetInput("date (dd/mm/yy) (empty for today)", DateHelper::ToStringDDMMYY(DateHelper::GetToday())));
+        auto amount = GetInput("amount (in cents: 10000 -> $100.00)");
+        auto description = GetInput("description");
+        auto tag = tag_from_string(GetInput("tag (empty for none)", "NONE"));
 
         Transaction transaction({ 0, "", (int)(stof(amount) * 100), description, DateHelper::ConvertToRFC(date), tag, true });
 
         DataManager::AddTransaction(transaction);
     }
 
-    void snapshot_menu()
+    void Snapshots()
     {
         string choice =
-            get_input(fmt::format("please pick an option:\n{}\n{}\n", "1: weekly", "2: pay cycle", "0: back"));
-        string show = get_input(fmt::format("show transactions?:\n{}\n{}\n", "0: no", "1: yes"));
+            GetInput(fmt::format("please pick an option:\n{}\n{}\n", "1: weekly", "2: pay cycle", "0: back"));
+        string show = GetInput(fmt::format("show transactions?:\n{}\n{}\n", "0: no", "1: yes"));
 
         DataManager::GenerateSnapshots(stoi(choice), stoi(show));
     }
